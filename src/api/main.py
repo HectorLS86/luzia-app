938d66b49763329ef15767d10e3e60853496eb97
HectorLS86 <hectorlopezfv@gmail.com>
2025-05-28 21:31:26 +0200

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,28 +1,38 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import HTMLResponse
+import boto3
 import os
-import os
-bucket_name = os.getenv("AWS_BUCKET_NAME")
 
 app = FastAPI()
 
-# Crear la carpeta de uploads si no existe
-os.makedirs('subidas', exist_ok=True)
+# Cargar el nombre del bucket y credenciales desde variables de entorno
+bucket_name = os.getenv("AWS_BUCKET_NAME")
+region = os.getenv("AWS_REGION")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+# Configurar el cliente S3
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=region
+)
 
 @app.get("/", response_class=HTMLResponse)
 async def leer_raiz():
     return """
-    <h1>Bienvenido a Luzia!</h1>
+    <h1>Bienvenido a Luzia</h1>
     <form action="/subir" enctype="multipart/form-data" method="post">
-        <input name="archivo" type="file">
-        <input type="submit" value="Subir">
+    <input name="archivo" type="file">
+    <input type="submit" value="Subir">
     </form>
     """
 
 @app.post("/subir")
 async def subir_archivo(archivo: UploadFile = File(...)):
-    ruta = os.path.join("subidas", archivo.filename)
-    with open(ruta, "wb") as buffer:
-        contenido = await archivo.read()
-        buffer.write(contenido)
-    return {"mensaje": f"Archivo '{archivo.filename}' subido exitosamente!"}
+    contenido = await archivo.read()
+    # Subir el archivo a S3
+    s3.put_object(Bucket=bucket_name, Key=archivo.filename, Body=contenido)
+    return {"mensaje": f"Archivo {archivo.filename} subido correctamente a S3!"}
+
